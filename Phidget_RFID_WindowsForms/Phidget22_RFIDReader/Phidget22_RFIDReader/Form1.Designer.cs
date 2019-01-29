bd12a5b1d7cd73d09c17dc4984ab49a525a634b6
Freek393331 <37250789+Freek@users.noreply.github.com>
2019-01-29 15:49:56 +0100

    Added WindowsFormsApplication

--- Phidget_RFID_WindowsForms/Phidget22_RFIDReader/Phidget22_RFIDReader/Form1.Designer.cs
+++ Phidget_RFID_WindowsForms/Phidget22_RFIDReader/Phidget22_RFIDReader/Form1.Designer.cs
@@ -28,21 +28,72 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.checkBox1 = new System.Windows.Forms.CheckBox();
             this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(19, 30);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(35, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "label1";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(19, 78);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(35, 13);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "label2";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(19, 126);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(35, 13);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "label3";
+            // 
+            // checkBox1
+            // 
+            this.checkBox1.AutoSize = true;
+            this.checkBox1.Location = new System.Drawing.Point(22, 174);
+            this.checkBox1.Name = "checkBox1";
+            this.checkBox1.Size = new System.Drawing.Size(66, 17);
+            this.checkBox1.TabIndex = 3;
+            this.checkBox1.Text = "Antenna";
+            this.checkBox1.UseVisualStyleBackColor = true;
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(290, 230);
+            this.Controls.Add(this.checkBox1);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
             this.Name = "Form1";
             this.Text = "Form1";
             this.Load += new System.EventHandler(this.Form1_Load);
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.CheckBox checkBox1;
     }
 }
 
--- Phidget_RFID_WindowsForms/Phidget22_RFIDReader/Phidget22_RFIDReader/Form1.cs
+++ Phidget_RFID_WindowsForms/Phidget22_RFIDReader/Phidget22_RFIDReader/Form1.cs
@@ -14,6 +14,7 @@ namespace Phidget22_RFIDReader
 {
     public partial class Form1 : Form
     {
+        private RFID rfid;
         public Form1()
         {
             InitializeComponent();
@@ -21,7 +22,69 @@ namespace Phidget22_RFIDReader
 
         private void Form1_Load(object sender, EventArgs e)
         {
+            label1.Text = "tag:                    -";
+            label2.Text = "Status:                 Not Connected";
+            label3.Text = "Serial Number:          -";
 
+            checkBox1.Checked = false;
+            checkBox1.Enabled = false;
+
+            rfid = new RFID();
+
+            rfid.Attach += new AttachEventHandler(rfid_Attach);
+            rfid.Detach += new DetachEventHandler(rfid_Detach);
+            rfid.Error += new ErrorEventHandler(rfid_Error);
+
+            rfid.Tag += new RFIDTagEventHandler(rfid_Tag);
+            rfid.TagLost += new RFIDTagLostEventHandler(rfid_TagLost);
+
+            rfid.Open();
         }
+
+
+        private void rfid_Tag(object sender, RFIDTagEventArgs e)
+        {
+            label1.Text = "Tag:              " + e.Tag;
+        }
+
+        private void rfid_TagLost(object sender, RFIDTagLostEventArgs e)
+        {
+            label1.Text = "Tag:              -";
+        }
+
+
+        void rfid_Attach(object sender, AttachEventArgs e)
+        {
+            label1.Text = "Tag:              -";
+            label2.Text = "Status:           Connected";
+            label3.Text = "Serial Number:    " + rfid.DeviceSerialNumber.ToString();
+
+            checkBox1.Checked = rfid.AntennaEnabled;
+            checkBox1.Enabled = true;
+        }
+
+        private void rfid_Detach(object sender, DetachEventArgs e)
+        {
+            label1.Text = "Tag:              -";
+            label2.Text = "Status:           Not Connected";
+            label3.Text = "Serial Number:    -";
+
+            checkBox1.Checked = false;
+            checkBox1.Enabled = false;
+        }
+
+        private void rfid_Error(object sender, ErrorEventArgs e)
+        {
+            MessageBox.Show(e.Description);
+
+            rfid.Close();
+            this.Close();
+        }
+
+        private void checkBox1_CheckedChanged(object sender, EventArgs e)
+        {
+            rfid.AntennaEnabled = checkBox1.Checked;
+        }
+
     }
 }
